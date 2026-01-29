f4044978e3da37f26161222c86c95eb9810d637e
Vruttant Lihitkar <vruttantlihitkar@gmail.com>
2026-01-29 09:38:00 +0530

    Time: 489 ms (39.46%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,1 +1,1 @@
-select   activity_date as day , count(distinct user_id) as active_users from activity     where activity_date BETWEEN '2019-06-28' AND '2019-07-27' group by  activity_date
+select activity_date day,count(distinct user_id) active_users from activity where activity_date BETWEEN '2019-06-28' AND '2019-07-27' group by activity_date
